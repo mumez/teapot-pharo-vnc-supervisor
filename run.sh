cada770af78512aa066f8dbbc748c73581c3066f
Masashi Umezawa <ume@softumeya.com>
2021-02-09 23:48:34 +0900

    Updated VNC port number

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 docker build -t my_teapot .
-docker run --name my_teapot01 -d -p 5901:5901 -p 6901:6901 -p 9000:9000 my_teapot
+docker run --name my_teapot01 -d -p 5900:5900 -p 6901:6901 -p 9000:9000 my_teapot
 
-#docker run --name my_teapot -d -p 5901:5901 -p 6901:6901 -p 9000:9000 mumez/teapot-pharo-vnc-supervisor
+#docker run --name my_teapot -d -p 5900:5900 -p 6901:6901 -p 9000:9000 mumez/teapot-pharo-vnc-supervisor
